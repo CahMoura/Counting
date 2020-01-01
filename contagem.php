88f48557577a51561b1d04c89b09714dd34bd7f5
Caroline Moura <54120284+CahMoura@users.noreply.github.com>
2019-12-31 21:12:53 -0300

    Add files via upload

---
+++ contagem.php
@@ -0,0 +1,17 @@
+<?php
+for ($i =1;$i <= 10;$i++){
+echo "$i ";
+}
+echo "_";
+for ($i=10;$i>=1;$i--){
+    echo " $i ";
+}
+echo "_";
+for ($i=0;$i<=50;$i+=5){
+    echo " $i ";
+}
+echo "_";
+for ($i=20;$i>=0;$i-=2){
+    print " $i ";
+}
+?>
